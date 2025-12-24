c72d2dc93a34ce044724c0122e8260f2907d1bf6
SagorIslamOfficial <alaminislam1274@gmail.com>
2025-12-25 04:54:50 +0600

    feat: add HasUuids trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Modules\HR\Employee\Models\Employee;
 use App\Traits\StatusManagement;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -25,7 +26,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, HasRoles, Notifiable, SoftDeletes, StatusManagement, TwoFactorAuthenticatable;
+    use HasFactory, HasRoles, HasUuids, Notifiable, SoftDeletes, StatusManagement, TwoFactorAuthenticatable;
 
     /**
      * The status options available for users.
