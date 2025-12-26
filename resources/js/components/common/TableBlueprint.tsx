9293d1792607975ec8e64a5080f8c5101e051aa1
SagorIslamOfficial <alaminislam1274@gmail.com>
2025-12-26 06:39:38 +0600

    feat: add TableBlueprint component for customizable data tables

---
+++ resources/js/components/common/TableBlueprint.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { DataTable } from '@/components/ui/data-table';
+import { ColumnDef } from '@tanstack/react-table';
+
+interface TableBlueprintProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+    searchPlaceholder?: string;
+    globalSearchKeys?: (keyof TData)[];
+    className?: string;
+    getRowProps?: (row: TData) => React.HTMLAttributes<HTMLTableRowElement>;
+}
+
+export function TableBlueprint<TData, TValue>({
+    columns,
+    data,
+    searchPlaceholder = 'Search...',
+    globalSearchKeys,
+    className,
+    getRowProps,
+}: TableBlueprintProps<TData, TValue>) {
+    return (
+        <DataTable
+            columns={columns}
+            data={data}
+            searchPlaceholder={searchPlaceholder}
+            globalSearchKeys={globalSearchKeys}
+            className={className}
+            getRowProps={getRowProps}
+        />
+    );
+}
